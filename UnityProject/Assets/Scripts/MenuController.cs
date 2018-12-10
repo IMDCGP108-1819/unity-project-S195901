30b149c4f1d083a8eca2d0979a4f5eb7179ac5ae
S195901 <45767970+S195901@users.noreply.github.com>
2018-12-10 17:53:03 +0000

    Add files via upload

---
+++ UnityProject/Assets/Scripts/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/*This script allows the menu to be interactable with two simple actions. Pressing the Start button, the player will load into the game.
+ * If the player clicks Exit, the application will close*/
+
+public class MenuController : MonoBehaviour
+{
+
+    public Button StartBtn;
+    public Button ExitBtn;
+
+    public CanvasGroup MainMenuGroup;
+
+    public void LoadLevel(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+        StartBtn.interactable = false;
+        ExitBtn.interactable = false;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    /* This script controls how the Pterodactyl is controlled by the player. The TorqueForce enables the sprite to spin AND slow down. I decided
+     * to use TorqueForce instead of an animation to make the spinning challenging, yet rewarding; also allowing for different spinning speeds.*/
+    public Rigidbody2D rigidBody;
+    public float TorqueForce = 10.0f;
+    public Text scoreText;
+
+    public int score;
+
+
+    void Start()
+    {
+        score = 0;
+        UpdateScore ();
+    
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        rigidBody.AddTorque(TorqueForce);
+        AddScore(1);
+      
+    }
+
+    /* This part of the code basically controls the scorekeeping side of things. It will keep track of how many times the player has clicked
+     the sprite, and represent it with text.*/
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore ()
+    {
+        scoreText.text = "Clicks: " + score;
+    }
+}
+
